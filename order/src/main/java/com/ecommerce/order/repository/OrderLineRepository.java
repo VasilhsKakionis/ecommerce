222b905d7143b80525a0ac66505cd672f744d317
Vasilis Kakionis <vasilhs_kakionis@yahoo.gr>
2025-11-14 23:38:53 +0200

    Update OrderLineRepository.java

--- order/src/main/java/com/ecommerce/order/repository/OrderLineRepository.java
+++ order/src/main/java/com/ecommerce/order/repository/OrderLineRepository.java
@@ -4,10 +4,7 @@ import com.ecommerce.order.model.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
 
-    List<OrderLine> findByOrderOrderId(Long orderId);
 }
